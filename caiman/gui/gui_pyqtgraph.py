c39539364466eb0317386be3e95ce62e0eef5795
agiovann <andrea.giovannucci@gmail.com>
2018-11-15 11:17:04 -0500

    added metric controls to GUI

--- caiman/gui/gui_pyqtgraph.py
+++ caiman/gui/gui_pyqtgraph.py
@@ -5,6 +5,7 @@ import caiman as cm
 from caiman.source_extraction.cnmf.cnmf import load_CNMF
 import cv2
 import scipy
+import os
 
 try:
     cv2.setNumThreads(1)
@@ -21,8 +22,6 @@ try:
 except NameError:
     print('Not launched under iPython')
 
-#%%
-
 def make_color_img(img, gain=255, out_type=np.uint8):
     min = img.min()
     max = img.max()
@@ -31,10 +30,37 @@ def make_color_img(img, gain=255, out_type=np.uint8):
     img = np.dstack([img]*3)
     return img
 
+
+
+# load estimates and movie image
+# base_folder = '/mnt/ceph/neuro/DataForPublications/DATA_PAPER_ELIFE/WEBSITE/'
+base_folder = '/Users/agiovannucci/SOFTWARE/CaImAn/YST/YST'
+mov = cm.load(os.path.join(base_folder,'Yr_d1_200_d2_256_d3_1_order_C_frames_3000_.mmap'))
+Cn = cm.load(os.path.join(base_folder,'projections/correlation_image.tif'))
+cnm_obj = load_CNMF(os.path.join(base_folder,'Yr_d1_200_d2_256_d3_1_order_C_frames_3000__cnmf_perf_web_after_analysis.hdf5'))
+estimates = cnm_obj.estimates
+estimates.restore_discarded_components()
+estimates.img_components = estimates.A.toarray().reshape((estimates.dims[0], estimates.dims[1],-1), order='F').transpose([2,0,1])
+estimates.cms = np.array([scipy.ndimage.measurements.center_of_mass(comp) for comp in estimates.img_components])
+estimates.idx_components = np.arange(estimates.nr)
+estimates.idx_components_bad = np.array([])
+estimates.background_image = make_color_img(Cn)
+# Generate image data
+estimates.img_components /= estimates.img_components.max(axis=(1,2))[:,None,None]
+estimates.img_components *= 255
+estimates.img_components = estimates.img_components.astype(np.uint8)
+
+
+
+#%%
+
+
 def draw_contours():
-    global thrshcomp_line, selected_components, img, background_image
-    contours = [cv2.findContours(cv2.threshold(img, np.int(thrshcomp_line.value()), 255, 0)[1], cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[1] for img in components[selected_components]]
-    bkgr_contours = background_image.copy()
+    global thrshcomp_line, estimates, cnm_obj, img
+    contours = [cv2.findContours(cv2.threshold(img, np.int(thrshcomp_line.value()), 255, 0)[1], cv2.RETR_TREE,
+                                 cv2.CHAIN_APPROX_SIMPLE)[1] for img in estimates.img_components[estimates.idx_components]]
+    bkgr_contours = estimates.background_image.copy()
+
     cv2.drawContours(bkgr_contours, sum(contours[0::6], []), -1, (255, 0, 0), 1)
     cv2.drawContours(bkgr_contours, sum(contours[1::6], []), -1, (0, 255, 0), 1)
     cv2.drawContours(bkgr_contours, sum(contours[2::6], []), -1, (0, 0, 255), 1)
@@ -42,20 +68,6 @@ def draw_contours():
     cv2.drawContours(bkgr_contours, sum(contours[4::6], []), -1, (255, 0, 255), 1)
     cv2.drawContours(bkgr_contours, sum(contours[5::6], []), -1, (0, 255, 255), 1)
     img.setImage(bkgr_contours, autoLevels=False)
-
-
-# load estimates and movie image
-mov = cm.load('/mnt/ceph/neuro/DataForPublications/DATA_PAPER_ELIFE/WEBSITE/YST/Yr_d1_200_d2_256_d3_1_order_C_frames_3000_.mmap')
-Cn = cm.load('/mnt/ceph/neuro/DataForPublications/DATA_PAPER_ELIFE/WEBSITE/YST/projections/correlation_image.tif')
-background_image = make_color_img(Cn)
-cnm_obj = load_CNMF('/mnt/ceph/neuro/DataForPublications/DATA_PAPER_ELIFE/WEBSITE/YST/Yr_d1_200_d2_256_d3_1_order_C_frames_3000__cnmf_perf_web_after_analysis.hdf5')
-estimates = cnm_obj.estimates
-estimates.restore_discarded_components()
-estimates.img_components = estimates.A.toarray().reshape((estimates.dims[0], estimates.dims[1],-1), order='F').transpose([2,0,1])
-estimates.cms = np.array([scipy.ndimage.measurements.center_of_mass(comp) for comp in estimates.img_components])
-selected_components = np.arange(estimates.nr)
-
-
 # Interpret image data as row-major instead of col-major
 # pg.setConfigOptions(imageAxisOrder='row-major')
 
@@ -87,22 +99,55 @@ win.addItem(hist)
 
 hist_cnn = pg.HistogramLUTItem()
 img_cnn = pg.ImageItem()
-img_cnn.setImage(np.atleast_2d(estimates.cnn_preds), autoLevels=False)
+img_cnn.setImage(estimates.cnn_preds[np.newaxis,:], autoLevels=False)
 hist_cnn.setImageItem(img_cnn)
+hist_cnn.setObjectName('metric_CNN')
 win.addItem(hist_cnn)
+
+hist_rval = pg.HistogramLUTItem()
+img_rval = pg.ImageItem()
+img_rval.setImage(estimates.cnn_preds[np.newaxis,:], autoLevels=False)
+hist_rval.setImageItem(img_rval)
+hist_rval.setObjectName('metric_rval')
+win.addItem(hist_rval)
+
+
+hist_SNR = pg.HistogramLUTItem()
+img_SNR = pg.ImageItem()
+img_SNR.setImage(estimates.SNR_comp[np.newaxis,:], autoLevels=False)
+hist_SNR.setImageItem(img_SNR)
+hist_SNR.setObjectName('metric_SNR')
+win.addItem(hist_SNR)
+
 
 
 
 def changed_quality_metrics(event):
-    global selected_components
-    low_cnn, high_cnn = event.getLevels()
-    selected_components = np.where(estimates.cnn_preds >= low_cnn)[0]
-    discarded_components = np.where(estimates.cnn_preds <= low_cnn)[0]
-    selected_components = np.setdiff1d(selected_components, discarded_components)
+    global estimates, cnm_obj, event_
+    params = cnm_obj.params
+
+    if event.objectName() == 'metric_CNN':
+        cnn_lowest, min_cnn_thr = event.getLevels()
+        params.quality.update({'cnn_lowest': cnn_lowest,'min_cnn_thr': min_cnn_thr})
+    elif event.objectName() == 'metric_SNR':
+        SNR_lowest, min_SNR = event.getLevels()
+        params.quality.update({'SNR_lowest': SNR_lowest, 'min_SNR': min_SNR})
+    elif event.objectName() == 'metric_rval':
+        rval_lowest, rval_thr = event.getLevels()
+        params.quality.update({'rval_lowest': rval_lowest,'rval_thr': rval_thr})
+    else:
+        raise Exception('Unknown Metric Object')
+    estimates.filter_components(mov, params, dview=None)
+
+    # selected_components = np.where(estimates.cnn_preds >= low_cnn)[0]
+    # discarded_components = np.where(estimates.cnn_preds <= low_cnn)[0]
+    # estimates.idx_components = np.setdiff1d(selected_components, discarded_components)
     draw_contours()
 
 
-hist_cnn.sigLevelsChanged.connect(test)
+hist_cnn.sigLevelsChanged.connect(changed_quality_metrics)
+hist_rval.sigLevelsChanged.connect(changed_quality_metrics)
+hist_SNR.sigLevelsChanged.connect(changed_quality_metrics)
 
 
 
@@ -122,15 +167,8 @@ p2.setMaximumHeight(250)
 win.resize(800, 800)
 win.show()
 
-
-# Generate image data
-components /= components.max(axis=(1,2))[:,None,None]
-components *= 255
-components = components.astype(np.uint8)
-
-
 draw_contours()
-hist.setLevels(background_image.min(), background_image.max())
+hist.setLevels(estimates.background_image.min(), estimates.background_image.max())
 
 
 # set position and scale of image
@@ -172,9 +210,9 @@ def imageHoverEvent(event):
     global x,y,i,j,val
     pos = event.pos()
     i, j = pos.y(), pos.x()
-    i = int(np.clip(i, 0, background_image.shape[0] - 1))
-    j = int(np.clip(j, 0, background_image.shape[1] - 1))
-    val = background_image[i, j, 0]
+    i = int(np.clip(i, 0, estimates.background_image.shape[0] - 1))
+    j = int(np.clip(j, 0, estimates.background_image.shape[1] - 1))
+    val = estimates.background_image[i, j, 0]
     ppos = img.mapToParent(pos)
     x, y = ppos.x(), ppos.y()
 
@@ -186,11 +224,12 @@ img.hoverEvent = imageHoverEvent
 
 def mouseClickEvent(event):
     global x, y, i, j, val
-    distances = np.sum(((x,y)-cms)**2, axis=1)**0.5
-    closest_component = np.argmin(distances)
-    p2.plot(estimates.C[closest_component], clear=True)
-    p1.setTitle("pos: (%0.1f, %0.1f)  component: %d  value: %g dist:%f" % (x, y, closest_component, val,distances[closest_component]))
+    distances = np.sum(((x,y)-estimates.cms[estimates.idx_components])**2, axis=1)**0.5
+    estimates.components_to_plot = estimates.idx_components[np.argmin(distances)]
+    p2.plot(estimates.C[estimates.components_to_plot], clear=True)
 
+    p1.setTitle("pos: (%0.1f, %0.1f)  component: %d  value: %g dist:%f" % (x, y, estimates.components_to_plot,
+                                                                           val, distances[estimates.components_to_plot]))
 p1.vb.mouseClickEvent = mouseClickEvent
 
 
